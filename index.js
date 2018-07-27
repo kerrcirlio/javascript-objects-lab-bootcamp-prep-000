647fd07fc7b0a9a76f7a31bacd4f887bc4b78f71
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-27 01:34:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
    return object
 }
 
-function deleteFromObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
   return object
 }
+
